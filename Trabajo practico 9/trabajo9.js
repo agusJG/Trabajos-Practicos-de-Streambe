e7195333f03d645e4f23b33b34999915a4d5d7c4
agusJG <163325039+agusJG@users.noreply.github.com>
2024-11-08 10:30:40 -0300

    Add files via upload

---
+++ Trabajo practico 9/trabajo9.js
@@ -0,0 +1,33 @@
+let objeto_demo={
+    uno:1,
+    dos:2,
+    tres:3
+};
+let uno = "tres"
+objeto_demo['dos']
+objeto_demo[uno]
+
+let capitales={
+    argentina:'Buenos aires',
+    uruguay:'montevideo'
+};
+let lugar = 'uruguay'
+// capitales['lugar'] = undefined
+// capitales['lugar'] = undefined
+// capitales[lugar]
+// 'montevideo'
+// capitales['argentina']
+// 'Buenos aires'
+// capitales.argentina
+// 'Buenos aires'
+// capitales[argentina]
+// VM522:1 Uncaught ReferenceError: argentina is not defined
+//     at <anonymous>:1:11
+let registro={
+    Usuario:"Newusuario",
+    Edad:23,
+    Direccion:"falsa",
+    Fechadenacimiento:"XX/XX/XXXX",
+    Contraseña:"Fakepassword"
+}
+
